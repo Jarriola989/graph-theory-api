3a5ac9d0bec554ec91e546dda33c8fc69ea53159
janeth <jarri023@fiu.edu>
2020-03-26 00:54:15 -0400

    exposed post request to create graph

--- routes.js
+++ routes.js
@@ -1,3 +1,12 @@
-// export function routes(app){
+import { createGraph } from "./create-graph";
 
-// }
+export function routes(app) {
+  app.post("/create-graph", function(req, res) {
+    const graph = createGraph(req.body.nodeCount, req.body.isDirected);
+    return res.status(200).send({
+      success: true,
+      message: "Graph created successfully",
+      graph: graph
+    });
+  });
+}
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { routes } from "./routes";
 
 const app = express();
 const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+routes(app);
 
 app.listen(port, () =>
   console.log(`Graph Theory app listening on port ${port}!`)
