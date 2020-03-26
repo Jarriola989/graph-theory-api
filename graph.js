d729099abceffd6a995ff5b9ef84c19d7f622bf8
janeth <jarri023@fiu.edu>
2020-03-26 00:46:49 -0400

    graph class

--- graph.js
+++ graph.js
@@ -1,5 +1,38 @@
-export let graph = {
-  vertices: 0,
-  edges: 0,
-  matrix: [] // this will be a 2d array
-};
+export let node_list = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+
+export class Graph {
+  constructor(nodeCount, edgeCount, isDirected) {
+    this.isDirected = isDirected;
+    this.nodeCount = nodeCount;
+    this.nodes = [];
+    this.edgeCount = edgeCount;
+    this.edges = []; // this will be a 2d array
+  }
+}
