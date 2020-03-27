639871ae8f9b9e3c489734a7212c9bae6605296b
janeth <jarri023@fiu.edu>
2020-03-27 00:03:23 -0400

    nodeCount fix and stringified edges for to check if they exist

--- create-graph.js
+++ create-graph.js
@@ -1,27 +1,31 @@
-import { Graph, node_list } from "./graph";
+import { Graph, node_list } from "./graph.js";
 
 export function createGraph(nodeCount, isDirected) {
+  console.log(`is the graph directed? ${isDirected}`);
   const maxEdges = !isDirected
-    ? nodeCount(nodeCount - 1) / 2
-    : 2(nodeCount(nodeCount - 1) / 2);
+    ? (nodeCount * (nodeCount - 1)) / 2
+    : 2 * ((nodeCount * (nodeCount - 1)) / 2);
   const edgeCount = Math.floor(Math.random() * maxEdges + 1);
   const graph = new Graph(nodeCount, edgeCount, isDirected);
   for (let i = 0; i < nodeCount; i++) {
     graph.nodes.push(node_list[i]);
   }
   for (let i = 0; i < edgeCount; i++) {
+    console.log(`Node ${i + 1}/${edgeCount}`);
     let u = graph.nodes[Math.floor(Math.random() * nodeCount)];
     let v = graph.nodes[Math.floor(Math.random() * nodeCount)];
+    console.log(`first try: [${u},${v}]`);
     // Undirected graphs cannot have reverse edges and cannot point to themselves.
     while (
-      (!isDirected && graph.edges.includes([u, v])) ||
-      graph.edges.includes([v, u]) ||
-      u === v
+      !isDirected &&
+      (JSON.stringify(graph.edges).includes(JSON.stringify([u, v])) ||
+        JSON.stringify(graph.edges).includes(JSON.stringify([v, u])) ||
+        u === v)
     ) {
       u = graph.nodes[Math.floor(Math.random() * nodeCount)];
       v = graph.nodes[Math.floor(Math.random() * nodeCount)];
     }
-    while (isDirected && graph.edges.includes([u, v])) {
+    while (isDirected && JSON.stringify(graph.edges).includes(JSON.stringify([u, v]))) {
       u = graph.nodes[Math.floor(Math.random() * nodeCount)];
       v = graph.nodes[Math.floor(Math.random() * nodeCount)];
     }
